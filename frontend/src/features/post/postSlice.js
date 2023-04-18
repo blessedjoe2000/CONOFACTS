7a591d7db4793a07019294b5322d3993c7361d0d
Joseph Ugiagbe <blessedjoe2000@yahoo.com>
2023-04-18 16:48:20 +0100

    fix icon

--- frontend/src/features/post/postSlice.js
+++ frontend/src/features/post/postSlice.js
@@ -4,13 +4,8 @@ import postService from "./postService";
 const initialState = {
   posts: [],
   isPending: false,
-<<<<<<< HEAD
   isError: false,
   isSuccess: false,
-=======
-  isSuccess: false,
-  isError: false,
->>>>>>> b04a3eff6aff7e6b055680047172c855e5de89e6
   message: "",
 };
 
@@ -19,12 +14,8 @@ export const createPost = createAsyncThunk(
   async (postData, thunkAPI) => {
     try {
       const token = thunkAPI.getState().auth.user.token;
-<<<<<<< HEAD
       const response = await postService.createPost(postData, token);
       return response.data;
-=======
-      return await postService.createPost(postData, token);
->>>>>>> b04a3eff6aff7e6b055680047172c855e5de89e6
     } catch (error) {
       const message =
         (error.response &&
@@ -38,11 +29,7 @@ export const createPost = createAsyncThunk(
 );
 
 export const getPosts = createAsyncThunk(
-<<<<<<< HEAD
   "posts/getAll",
-=======
-  "posts/getUserPost",
->>>>>>> b04a3eff6aff7e6b055680047172c855e5de89e6
   async (_, thunkAPI) => {
     try {
       const token = thunkAPI.getState().auth.user.token;
@@ -59,8 +46,6 @@ export const getPosts = createAsyncThunk(
   }
 );
 
-<<<<<<< HEAD
-=======
 export const getAllPosts = createAsyncThunk(
   "posts/getAllPost",
   async (_, thunkAPI) => {
@@ -78,7 +63,6 @@ export const getAllPosts = createAsyncThunk(
   }
 );
 
->>>>>>> b04a3eff6aff7e6b055680047172c855e5de89e6
 export const updatePost = createAsyncThunk(
   "posts/update",
   async (id, postData, thunkAPI) => {
@@ -119,13 +103,9 @@ export const postSlice = createSlice({
   name: "post",
   initialState,
   reducers: {
-<<<<<<< HEAD
-    reset: (state) => initialState,
-=======
     reset: (state) => {
       state = { ...initialState };
     },
->>>>>>> b04a3eff6aff7e6b055680047172c855e5de89e6
   },
   extraReducers: (builder) => {
     builder
@@ -133,16 +113,6 @@ export const postSlice = createSlice({
         state.isPending = true;
       })
       .addCase(createPost.fulfilled, (state, action) => {
-<<<<<<< HEAD
-        state.isPending = false;
-        state.isSuccess = true;
-        state.posts.push(action.payload);
-        state.message = "user created sucessfully";
-      })
-      .addCase(createPost.rejected, (state, action) => {
-        state.isPending = false;
-        state.isError = true;
-=======
         state.isSuccess = true;
         state.isPending = false;
         state.posts.push(action.payload);
@@ -151,28 +121,18 @@ export const postSlice = createSlice({
       .addCase(createPost.rejected, (state, action) => {
         state.isError = true;
         state.isPending = false;
->>>>>>> b04a3eff6aff7e6b055680047172c855e5de89e6
         state.message = action.payload;
       })
       .addCase(getPosts.pending, (state) => {
         state.isPending = true;
       })
       .addCase(getPosts.fulfilled, (state, action) => {
-<<<<<<< HEAD
-        state.isPending = false;
-        state.isSuccess = true;
-=======
         state.isSuccess = true;
         state.isPending = false;
->>>>>>> b04a3eff6aff7e6b055680047172c855e5de89e6
         state.isError = false;
         state.posts = action.payload;
       })
       .addCase(getPosts.rejected, (state, action) => {
-<<<<<<< HEAD
-        state.isPending = false;
-        state.isError = true;
-=======
         state.isError = true;
         state.isPending = false;
         state.message = action.payload;
@@ -190,22 +150,12 @@ export const postSlice = createSlice({
       .addCase(getAllPosts.rejected, (state, action) => {
         state.isError = true;
         state.isPending = false;
->>>>>>> b04a3eff6aff7e6b055680047172c855e5de89e6
         state.message = action.payload;
       })
       .addCase(updatePost.pending, (state) => {
         state.isPending = true;
       })
       .addCase(updatePost.fulfilled, (state, action) => {
-<<<<<<< HEAD
-        state.isPending = false;
-        state.isSuccess = true;
-        state.posts = action.payload;
-      })
-      .addCase(updatePost.rejected, (state, action) => {
-        state.isPending = false;
-        state.isError = true;
-=======
         state.isSuccess = true;
         state.isPending = false;
         state.posts = action.payload;
@@ -213,7 +163,6 @@ export const postSlice = createSlice({
       .addCase(updatePost.rejected, (state, action) => {
         state.isError = true;
         state.isPending = false;
->>>>>>> b04a3eff6aff7e6b055680047172c855e5de89e6
         state.message = action.payload;
       })
 
@@ -221,25 +170,15 @@ export const postSlice = createSlice({
         state.isPending = true;
       })
       .addCase(deletePost.fulfilled, (state, action) => {
-<<<<<<< HEAD
-        state.isPending = false;
-        state.isSuccess = true;
-=======
         state.isSuccess = true;
         state.isPending = false;
->>>>>>> b04a3eff6aff7e6b055680047172c855e5de89e6
         state.posts = state.posts.filter(
           (post) => post._id !== action.payload.id
         );
       })
       .addCase(deletePost.rejected, (state, action) => {
-<<<<<<< HEAD
-        state.isPending = false;
-        state.isError = true;
-=======
         state.isError = true;
         state.isPending = false;
->>>>>>> b04a3eff6aff7e6b055680047172c855e5de89e6
         state.message = action.payload;
       });
   },
