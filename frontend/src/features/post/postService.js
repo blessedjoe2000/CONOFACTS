677ac3cb36f87abef034a4b4f762576d13b63703
Joseph Ugiagbe <blessedjoe2000@yahoo.com>
2023-04-20 19:28:56 +0100

    fixes

--- frontend/src/features/post/postService.js
+++ frontend/src/features/post/postService.js
@@ -1,16 +1,5 @@
 import axios from "axios";
 
-<<<<<<< HEAD
-const API_URL = `conofacts/posts/`;
-
-const createPost = async (postData, token) => {
-  const config = {
-    Headers: {
-      Authourization: `Bearer ${token}`,
-    },
-  };
-  const response = await axios.post(API_URL, postData, config);
-=======
 const API_URL = `/conofacts/posts/`;
 
 const createPost = async (postData, token) => {
@@ -21,19 +10,11 @@ const createPost = async (postData, token) => {
   };
   const response = await axios.post(API_URL, postData, config);
 
->>>>>>> b04a3eff6aff7e6b055680047172c855e5de89e6
   return response.data;
 };
 
 const getPosts = async (token) => {
   const config = {
-<<<<<<< HEAD
-    Headers: {
-      Authourization: `Bearer ${token}`,
-    },
-  };
-  const response = await axios.get(API_URL, config);
-=======
     headers: {
       authorization: `Bearer ${token}`,
     },
@@ -46,19 +27,13 @@ const getPosts = async (token) => {
 const getAllPosts = async () => {
   const response = await axios.get(API_URL + "all");
 
->>>>>>> b04a3eff6aff7e6b055680047172c855e5de89e6
   return response.data;
 };
 
 const updatePost = async (postId, postData, token) => {
   const config = {
-<<<<<<< HEAD
-    Headers: {
-      Authourization: `Bearer ${token}`,
-=======
     headers: {
       authorization: `Bearer ${token}`,
->>>>>>> b04a3eff6aff7e6b055680047172c855e5de89e6
     },
   };
   const response = await axios.put(API_URL + postId, postData, config);
@@ -77,22 +52,14 @@ const updateInterest = async (postId, postData, token) => {
 
 const deletePost = async (postId, token) => {
   const config = {
-<<<<<<< HEAD
-    Headers: {
-      Authourization: `Bearer ${token}`,
-=======
     headers: {
       authorization: `Bearer ${token}`,
->>>>>>> b04a3eff6aff7e6b055680047172c855e5de89e6
     },
   };
   const response = await axios.delete(API_URL + postId, config);
   return response.data;
 };
 
-<<<<<<< HEAD
-const postService = { createPost, getPosts, updatePost, deletePost };
-=======
 const postService = {
   createPost,
   getPosts,
@@ -100,6 +67,5 @@ const postService = {
   deletePost,
   getAllPosts,
 };
->>>>>>> b04a3eff6aff7e6b055680047172c855e5de89e6
 
 export default postService;
