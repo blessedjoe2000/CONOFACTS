763fee0a5054704f6686f2f53ec49d6466e2d066
blessedjoe2000 <blessedjoe2000@yahoo.com>
2023-05-19 15:41:36 +0100

    file path

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
-
+const dirname = require("path");
+const fileURLToPath = require("url");
+const path = require("path");
 const { userRouter } = require("./routers/userRouter");
 const connectDb = require("./config/db");
 const errorHandler = require("./middleware/errorMiddleware");
@@ -12,6 +14,9 @@ connectDb();
 const app = express();
 app.use(cors());
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
+app.use(express.static(path.resolve(__dirname, "../frontend/build")));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
@@ -19,6 +24,9 @@ app.use("/conofacts/users", userRouter);
 app.use("/conofacts/posts", postRouter);
 
 app.use(errorHandler);
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../fronend/build", "index.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`Server is runing on port ${PORT}`);
